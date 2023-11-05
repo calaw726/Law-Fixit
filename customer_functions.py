ee1715201e7eb39a78e0157c14d3cc0879a8b97c
Charles Law <calaw726@gmail.com>
2023-11-04 20:50:47 -0400

    First Commit

---
+++ customer_functions.py
@@ -0,0 +1,154 @@
+import sqlite3
+import re
+import tkinter as tk
+from tkinter import messagebox
+
+# Create the Customers table (you should also create other tables similarly)
+connection = sqlite3.connect('mechanic_shop.db')
+cursor = connection.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Customers (
+        customer_id INTEGER PRIMARY KEY,
+        first_name TEXT,
+        last_name TEXT,
+        phone_number TEXT,
+        email TEXT
+    )
+''')
+connection.commit()
+
+def is_valid_email(email):
+    # Define a regular expression pattern for a valid email address
+    pattern = r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+    
+    # Use the re.match() function to check if the email matches the pattern
+    if re.match(pattern, email):
+        return True
+    else:
+        return False
+    
+def is_valid_phone_number(phone_number):
+    # Remove any non-numeric characters (e.g., spaces, dashes, parentheses)
+    cleaned_number = ''.join(filter(str.isdigit, phone_number))
+
+    # Check if the cleaned phone number contains only digits
+    if cleaned_number.isdigit():
+        # Check if the length of the cleaned number is within an acceptable range
+        if 7 <= len(cleaned_number) <= 15:
+            return True
+    return False
+
+def add_customer(first_name_entry, last_name_entry, phone_number_entry, email_entry, listbox):
+    first_name = first_name_entry.get()
+    last_name = last_name_entry.get()
+    phone_number = phone_number_entry.get()
+    email = email_entry.get()
+
+    first_name_entry.delete(0, tk.END)
+    last_name_entry.delete(0, tk.END)
+    phone_number_entry.delete(0, tk.END)
+    email_entry.delete(0, tk.END)
+    
+    if not first_name or not last_name:
+        # Required fields are empty
+        messagebox.showerror("Error", "First Name and Last Name are required fields.")
+    elif not is_valid_phone_number(phone_number):
+        # Phone number contains non-numeric characters
+        messagebox.showerror("Error", "Phone Number must be valid.")
+    elif not is_valid_email(email):
+        # Email is not in a valid format
+        messagebox.showerror("Error", "Invalid Email Address.")
+    else:
+        # All checks passed, add the customer to the database
+        cursor.execute('''
+            INSERT INTO Customers (first_name, last_name, phone_number, email)
+            VALUES (?, ?, ?, ?)
+        ''', (first_name, last_name, phone_number, email))
+        connection.commit()
+        refresh_customer_list(listbox)
+        messagebox.showinfo("Success", "Customer added successfully.")
+
+# Function to edit a customer
+def modify_customer(customer_listbox):
+
+    # Get the selected item from the Listbox
+    selected_index = customer_listbox.curselection()
+
+    if not selected_index:
+        messagebox.showerror("Error", "Please select a customer to edit.")
+        return
+
+    # Extract the customer_id from the selected item
+    customer_entry = customer_listbox.get(selected_index)
+
+    try:
+        customer_id = int(customer_entry.split("ID: ")[1].split(",")[0])
+        existing_phone = customer_entry.split("Phone: ")[1].split(",")[0]
+        existing_email = customer_entry.split("Email: ")[1].split(",")[0]
+    except (ValueError, IndexError):
+        messagebox.showerror("Error", "Unable to extract customer ID.")
+        return
+    
+    # Create an edit dialog window
+    edit_window = tk.Toplevel()
+    edit_window.title("Edit Customer")# Create an edit dialog window
+    
+    # Create Entry widgets for the new phone and email
+    phone_label = tk.Label(edit_window, text="New Phone:")
+    phone_label.pack()
+    new_phone_entry = tk.Entry(edit_window)
+    new_phone_entry.insert(0, existing_phone)
+    new_phone_entry.pack()
+
+    email_label = tk.Label(edit_window, text="New Email:")
+    email_label.pack()
+    new_email_entry = tk.Entry(edit_window)
+    new_email_entry.insert(0, existing_email)
+    new_email_entry.pack()
+
+    # Function to save changes and update the database
+    def save_changes():
+        new_phone = new_phone_entry.get()
+        new_email = new_email_entry.get()
+
+        if not is_valid_phone_number(new_phone):
+            messagebox.showerror("Error", "Invalid Phone Number.")
+            return
+
+        if not is_valid_email(new_email):
+            messagebox.showerror("Error", "Invalid Email Address.")
+            return
+
+        # Update the customer's phone and email in the database
+        try:
+            # Update the customer's phone and email in the database
+            cursor.execute('''
+                UPDATE Customers
+                SET phone_number = ?, email = ?
+                WHERE customer_id = ?
+            ''', (new_phone, new_email, customer_id))
+
+            # Commit the changes to the database
+            connection.commit()
+        except sqlite3.Error as e:
+            print("Error updating customer phone and email:", e)
+        edit_window.destroy()
+        refresh_customer_list(customer_listbox)
+
+        messagebox.showinfo("Success", "Customer updated successfully.")
+
+    # Button to save changes
+    save_button = tk.Button(edit_window, text="Save", command=save_changes)
+    save_button.pack()
+
+    refresh_customer_list(customer_listbox)
+
+# Function to refresh the customer list
+def refresh_customer_list(listbox):
+    cursor.execute('SELECT * FROM Customers')
+    customers = cursor.fetchall()
+    listbox.delete(0, tk.END)
+    for customer in customers:
+        customer_info = f"ID: {customer[0]}, Name: {customer[1]} {customer[2]}, Phone: {customer[3]}, Email: {customer[4]}"
+        listbox.insert(tk.END, customer_info)
+
---
+++ fixit.py
@@ -0,0 +1,247 @@
+import sqlite3
+import tkinter as tk
+from tkinter import messagebox
+from PIL import ImageTk, Image
+from customer_functions import *
+
+
+# Initialize the SQLite database
+connection = sqlite3.connect('mechanic_shop.db')
+cursor = connection.cursor()
+
+
+
+def center_window(window, width, height):
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2
+
+    window.geometry(f'{width}x{height}+{x}+{y}')
+
+def load_main_frame():
+    # Create the main frame
+    welcome_frame = tk.Frame(root, width=window_width, height=window_height, bg=bg_color)
+    welcome_frame.grid(row=0, column=0, sticky="nesw")
+    welcome_frame.pack_propagate(False)
+
+    # Create the logo
+    logo_img = ImageTk.PhotoImage(get_logo())
+    logo_widget = tk.Label(welcome_frame, image=logo_img, bg=bg_color)
+    logo_widget.image = logo_img
+    logo_widget.pack()
+
+    # Create the buttons
+    edit_tables_button = tk.Button(
+        welcome_frame,
+        text="Edit Tables",
+        font=("Calibri", 48),
+        fg="Black",
+        cursor="hand2",
+        activebackground=button_pressed_color,
+        activeforeground="white",
+        command=lambda:load_edit_customer(),
+        relief=tk.FLAT,
+        borderwidth=0
+    ).pack()
+
+def load_edit_customer():
+    edit_customer = tk.Frame(root, width=window_width, height=window_height, bg=bg_color)
+    edit_customer.grid(row=0, column=0, sticky="nesw")
+    edit_customer.pack_propagate(False)
+
+    # Create input fields and buttons for managing customers
+    first_name_label = tk.Label(
+        edit_customer,
+        text="First Name",
+        bg = bg_color,
+        fg="white",
+        font=("Calibri", 12)
+    )
+    first_name_entry = tk.Entry(edit_customer)
+
+    last_name_label = tk.Label(
+        edit_customer,
+        text="Last Name",
+        bg = bg_color,
+        fg="white",
+        font=("Calibri", 12)
+    )
+    last_name_entry = tk.Entry(edit_customer)
+
+    phone_number_label = tk.Label(
+        edit_customer,
+        text="Phone Number",
+        bg=bg_color,
+        fg="white",
+        font=("Calibri", 12)
+    )
+    phone_number_entry = tk.Entry(edit_customer)
+
+    email_label = tk.Label(
+        edit_customer,
+        text="Email",
+        bg=bg_color,
+        fg="white",
+        font=("Calibri", 12)
+    )
+    email_entry = tk.Entry(edit_customer)
+
+    add_button = tk.Button(
+        edit_customer, 
+        text="Add Customer",
+        font=("Calibri", 18),
+        fg="black",
+        cursor="hand2",
+        activebackground=button_pressed_color,
+        activeforeground="white",
+        command=lambda: add_customer(
+            first_name_entry,
+            last_name_entry,
+            phone_number_entry,
+            email_entry,
+            customer_listbox),
+        relief=tk.FLAT,
+        borderwidth=0
+
+    )
+
+    edit_button = tk.Button(
+        edit_customer,
+        text="Edit Customer",
+        font=("Calibri", 18),
+        fg="black",
+        cursor="hand2",
+        activebackground=button_pressed_color,
+        activeforeground="white",
+        command=lambda: modify_customer(customer_listbox),
+        relief=tk.FLAT,
+        borderwidth=0
+    )
+
+    button_row =tk.Frame(edit_customer, bg=bg_color)
+    button_row.grid(row=1, column=0, sticky="nesw")
+    button_row.pack(side=tk.BOTTOM)
+
+    back_button = tk.Button(
+        button_row,
+        text="Back",
+        font=("Calibri", 18),
+        fg="black",
+        cursor="hand2",
+        activebackground=button_pressed_color,
+        activeforeground="white",
+        command=lambda: load_main_frame(),
+        relief=tk.FLAT,
+        borderwidth=0
+    )
+
+    vehicles_button = tk.Button(
+        button_row,
+        text="Vehicles",
+        font=("Calibri", 18),
+        fg="black",
+        cursor="hand2",
+        activebackground=button_pressed_color,
+        activeforeground="white",
+        command=lambda: load_edit_vehicle(),
+        relief=tk.FLAT,
+        borderwidth=0
+    )
+
+    services_button = tk.Button(
+        button_row,
+        text="Services",
+        font=("Calibri", 18),
+        fg="black",
+        cursor="hand2",
+        activebackground=button_pressed_color,
+        activeforeground="white",
+        command=lambda: load_edit_service(),
+        relief=tk.FLAT,
+        borderwidth=0
+    )
+
+    appointments_button = tk.Button(
+        button_row,
+        text="Appointments",
+        font=("Calibri", 18),
+        fg="black",
+        cursor="hand2",
+        activebackground=button_pressed_color,
+        activeforeground="white",
+        command=lambda: load_edit_appointment(),
+        relief=tk.FLAT,
+        borderwidth=0
+    )
+
+    invoices_button = tk.Button(
+        button_row,
+        text="Invoices",
+        font=("Calibri", 18),
+        fg="black",
+        cursor="hand2",
+        activebackground=button_pressed_color,
+        activeforeground="white",
+        command=lambda: load_edit_invoice(),
+        relief=tk.FLAT,
+        borderwidth=0
+    )
+
+    # Create a listbox to display customers
+    customer_listbox = tk.Listbox(edit_customer)
+    customer_listbox.pack(fill=tk.BOTH, padx=10, pady=10)
+
+    # Pack the input fields and buttons
+    first_name_label.pack()
+    first_name_entry.pack()
+    last_name_label.pack()
+    last_name_entry.pack()
+    phone_number_label.pack()
+    phone_number_entry.pack()
+    email_label.pack()
+    email_entry.pack()
+    add_button.pack()
+    edit_button.pack(padx=(10, 10))
+    back_button.pack(side = "left", padx=10)
+    vehicles_button.pack(side = "left", padx=10)
+    services_button.pack(side = "left", padx=10)
+    appointments_button.pack(side = "left", padx=10)
+    invoices_button.pack(side = "left", padx=10)
+
+    
+
+    # Refresh the customer list
+    refresh_customer_list(customer_listbox)
+
+
+def get_logo():
+    # Create the logo
+    original_logo = Image.open(r'src/assets/logo.jpg')
+    width, height = original_logo.size
+    new_width = width // 2
+    new_height = height // 2
+
+    # Resize the logo
+    resized_logo = original_logo.resize((new_width, new_height), Image.LANCZOS)
+    return resized_logo
+
+# Create the main window
+bg_color = "#08142E"
+button_color = "#1b4298"
+button_pressed_color = "#becff4"
+
+window_width = 800
+window_height = 600
+
+root = tk.Tk()
+root.title("Law-Fixit DBMS")
+center_window(root, window_width, window_height)
+root.eval("tk::PlaceWindow . center")
+
+
+load_main_frame()
+
+# Start the Tkinter main loop
+root.mainloop()
