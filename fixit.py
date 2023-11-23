1e8e5977db38ae31f42408d6163c481396b20e64
Charles Law <calaw726@gmail.com>
2023-11-23 12:48:11 -0500

    Added Service Functionality

--- fixit.py
+++ fixit.py
@@ -4,6 +4,7 @@ from tkinter import messagebox
 from PIL import ImageTk, Image
 from customer_functions import *
 from vehicle_functions import *
+from service_functions import *
 
 
 # Initialize the SQLite database
@@ -389,6 +390,187 @@ def load_edit_vehicle():
     # Refresh the vehicle list
     refresh_vehicle_list(vehicle_listbox)
 
+def load_edit_service():
+    
+
+    edit_service = tk.Frame(root, width=window_width, height=window_height, bg=bg_color)
+    edit_service.grid(row=0, column=0, sticky="nesw")
+    edit_service.pack_propagate(False)
+
+    # Create input fields and buttons for managing services
+    service_name_label = tk.Label(
+        edit_service,
+        text="Service Name",
+        bg = bg_color,
+        fg="white",
+        font=("Calibri", 12)
+    )
+    service_name_entry = tk.Entry(edit_service)
+
+    description_label = tk.Label(
+        edit_service,
+        text="Description",
+        bg = bg_color,
+        fg="white",
+        font=("Calibri", 12)
+    )
+    description_entry = tk.Entry(edit_service)
+
+    price_label = tk.Label(
+        edit_service,
+        text="Price",
+        bg=bg_color,
+        fg="white",
+        font=("Calibri", 12)
+    )
+    price_entry = tk.Entry(edit_service)
+
+    add_button = tk.Button(
+        edit_service,
+        text="Add Service",
+        font=("Calibri", 18),
+        fg="black",
+        cursor="hand2",
+        activebackground=button_pressed_color,
+        activeforeground="white",
+        command=lambda: add_service(
+            service_listbox,
+            service_name_entry,
+            description_entry,
+            price_entry),
+        relief=tk.FLAT,
+        borderwidth=0
+    )
+
+    get_description_button = tk.Button(
+        edit_service,
+        text="Get Description",
+        font=("Calibri", 18),
+        fg="black",
+        cursor="hand2",
+        activebackground=button_pressed_color,
+        command=lambda: get_description(
+            service_listbox
+        ),
+        relief=tk.FLAT,
+        borderwidth=0
+    )
+        
+
+    edit_button = tk.Button(
+        edit_service,
+        text="Edit Service",
+        font=("Calibri", 18),
+        fg="black",
+        cursor="hand2",
+        activebackground=button_pressed_color,
+        activeforeground="white",
+        command=lambda: modify_service(service_listbox),
+        relief=tk.FLAT,
+        borderwidth=0
+    )
+
+    delete_button = tk.Button(
+        edit_service,
+        text="Delete Service",
+        font=("Calibri", 18),
+        fg="black",
+        cursor="hand2",
+        activebackground=button_pressed_color,
+        command=lambda: delete_service(service_listbox),
+        relief=tk.FLAT,
+        borderwidth=0
+    )
+
+    button_row = tk.Frame(edit_service, bg=bg_color)
+    button_row.grid(row=1, column=0, sticky="nesw")
+    button_row.pack(side=tk.BOTTOM)
+
+    back_button = tk.Button(
+        button_row,
+        text="Back",
+        font=("Calibri", 18),
+        fg="black",
+        cursor="hand2",
+        activebackground=button_pressed_color,
+        activeforeground="white",
+        command=lambda: load_main_frame(),
+        relief=tk.FLAT,
+        borderwidth=0
+    )
+
+    customers_button = tk.Button(
+        button_row,
+        text="Customers",
+        font=("Calibri", 18),
+        fg="black",
+        cursor="hand2",
+        activebackground=button_pressed_color,
+        activeforeground="white",
+        command = lambda: load_edit_customer(),
+        relief=tk.FLAT,
+        borderwidth=0
+    )
+
+    vehicles_button = tk.Button(
+        button_row,
+        text="Vehicles",
+        font=("Calibri", 18),
+        fg="black",
+        cursor="hand2",
+        activebackground=button_pressed_color,
+        activeforeground = "white",
+        command=lambda: load_edit_vehicle(),
+        relief=tk.FLAT,
+        borderwidth=0
+    )
+
+    appointments_button = tk.Button(
+        button_row,
+        text="Appointments",
+        font=("Calibri", 18),
+        fg="black",
+        cursor="hand2",
+        activebackground=button_pressed_color,
+        command=lambda: load_edit_appointment(),
+        relief=tk.FLAT,
+        borderwidth=0
+    )
+
+    invoices_button = tk.Button(
+        button_row,
+        text="Invoices",
+        font=("Calibri", 18),
+        fg="black",
+        cursor="hand2",
+        activebackground=button_pressed_color,
+        command=lambda: load_edit_invoice(),
+        relief=tk.FLAT,
+        borderwidth=0
+    )
+
+    # Create a listbox to display services
+    service_listbox = tk.Listbox(edit_service)
+    service_listbox.pack(fill=tk.BOTH, padx=10, pady=10)
+
+    # Pack the input fields and buttons
+    service_name_label.pack()
+    service_name_entry.pack()
+    description_label.pack()
+    description_entry.pack()
+    price_label.pack()
+    price_entry.pack()
+    add_button.pack()
+    get_description_button.pack()
+    edit_button.pack(padx=(10, 10))
+    delete_button.pack(padx=(10, 10))
+    back_button.pack(side = "left", padx=10)
+    customers_button.pack(side = "left", padx=10)
+    vehicles_button.pack(side = "left", padx=10)
+    appointments_button.pack(side = "left", padx=10)
+    invoices_button.pack(side = "left", padx=10)
+
+    refresh_service_list(service_listbox)
 
 def get_logo():
     # Create the logo
