85a7736c8af5a075d43e96080e17024cd8c22107
Charles Law <calaw726@gmail.com>
2023-11-23 19:55:33 -0500

    Cleaned up code and added error checking for price inputs

--- service_functions.py
+++ service_functions.py
@@ -19,15 +19,26 @@ def valid_service_id(service_id):
         return False
     return True
 
+def valid_price(price):
+    try:
+        float(price)
+    except ValueError:
+        return False
+    return True
+
 def add_service(service_listbox, service_name_entry, description_entry, price_entry):
     service_name = service_name_entry.get()
     description = description_entry.get()
     price = price_entry.get()
 
     # Check if the required fields are empty
-    if not service_name or not description or not price:
+    if not all((service_name, description, price)):
         # Required fields are empty
         messagebox.showerror("Error", "All fields are required.")
+        return
+    if not valid_price(price):
+        # Invalid price
+        messagebox.showerror("Error", "Invalid price. Must be a number")
         return
     try:
         cursor.execute('''
@@ -35,14 +46,16 @@ def add_service(service_listbox, service_name_entry, description_entry, price_en
             VALUES (?, ?, ?)
         ''', (service_name, description, price))
         connection.commit()
+
     except sqlite3.Error as error:
         messagebox.showerror("Error", f"Error adding Service: {error}.")
+
+    # Refresh the service list
     refresh_service_list(service_listbox)
 
     # Clear the entry widgets
-    service_name_entry.delete(0, tk.END)
-    description_entry.delete(0, tk.END)
-    price_entry.delete(0, tk.END)
+    for entry in (service_name_entry, description_entry, price_entry):
+        entry.delete(0, tk.END)
 
     messagebox.showinfo("Success", "Service added successfully.")
 
@@ -78,31 +91,32 @@ def modify_service(service_listbox):
     edit_window.title("Edit Service")
 
     # Create Entry widgets for the new service name, description, and price
-    service_name_label = tk.Label(edit_window, text="Service Name:")
-    service_name_entry = tk.Entry(edit_window)
-    service_name_entry.insert(0, existing_service_name)
-    description_label = tk.Label(edit_window, text="Description:")
-    description_entry = tk.Entry(edit_window)
-    description_entry.insert(0, existing_description)
-    price_label = tk.Label(edit_window, text="Price:")
-    price_entry = tk.Entry(edit_window)
-    price_entry.insert(0, existing_price)
+    labels_and_entries = [
+        ("Service Name:", existing_service_name),
+        ("Description:", existing_description),
+        ("Price:", existing_price)
+    ]
 
-    # Pack the widgets
-    service_name_label.pack()
-    service_name_entry.pack()
-    description_label.pack()
-    description_entry.pack()
-    price_label.pack()
-    price_entry.pack()
+    entry_values = []
+
+    for label, default_value in labels_and_entries:
+        tk.Label(edit_window, text=label).pack()
+        entry = tk.Entry(edit_window)
+        entry.insert(0, default_value)
+        entry.pack()
+        entry_values.append(entry)
 
     def save_changes():
-        new_service_name = service_name_entry.get()
-        new_description = description_entry.get()
-        new_price = price_entry.get()
+        new_service_name = entry_values[0].get()
+        new_description = entry_values[1].get()
+        new_price = entry_values[2].get()
 
-        if not new_service_name or not new_description or not new_price:
+        if not all ((new_service_name, new_description, new_price)):
             messagebox.showerror("Error", "All fields are required.")
+            return
+        if not valid_price(new_price):
+            # Invalid price
+            messagebox.showerror("Error", "Invalid price. Must be a number")
             return
         try:
             cursor.execute('''
@@ -114,10 +128,12 @@ def modify_service(service_listbox):
         except sqlite3.Error as error:
             messagebox.showerror("Error", f"Error updating service: {error}.")
         edit_window.destroy()
+        refresh_service_list(service_listbox)
 
     # Create a button to save the changes
-    save_button = tk.Button(edit_window, text="Save", command=save_changes).pack()
-    refresh_service_list(service_listbox)
+    save_button = tk.Button(edit_window, text="Save", command=save_changes)
+    save_button.pack()
+    
 
 def delete_service(service_listbox):
     selected_index = service_listbox.curselection()
@@ -125,23 +141,23 @@ def delete_service(service_listbox):
     if not selected_index:
         messagebox.showerror("Error", "Please select a service to delete.")
         return
-    service_entry = service_listbox.get(selected_index)
 
     try:
+        # Extract the service ID from the selected listbox entry
+        service_entry = service_listbox.get(selected_index)
         service_id = (service_entry.split("ID: ")[1].split(",")[0])
-    except (ValueError, IndexError):
-        messagebox.showerror("Error", "Unable to extract service ID.")
-        return
 
-    try:
+        # Delete the service from the database
         cursor.execute('''
             DELETE FROM Services
             WHERE service_id = ?
         ''', (service_id,))
         connection.commit()
-    except sqlite3.Error as error:
+        refresh_service_list(service_listbox)
+    except (ValueError, IndexError, sqlite3.Error) as error:
         messagebox.showerror("Error", f"Error deleting service: {error}.")
-    refresh_service_list(service_listbox)
+    
+    
 
 def get_description(service_listbox):
     selected_index = service_listbox.curselection()
