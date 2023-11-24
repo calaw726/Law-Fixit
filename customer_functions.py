8154ee6b2e749df11efa1745740b1431e4bb7f29
Charles Law <calaw726@gmail.com>
2023-11-23 22:55:35 -0500

    Optimized Code

--- customer_functions.py
+++ customer_functions.py
@@ -22,21 +22,14 @@ def is_valid_email(email):
     pattern = r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
     
     # Use the re.match() function to check if the email matches the pattern
-    if re.match(pattern, email):
-        return True
-    else:
-        return False
+    return bool(re.fullmatch(pattern, email))
     
 def is_valid_phone_number(phone_number):
     # Remove any non-numeric characters (e.g., spaces, dashes, parentheses)
     cleaned_number = ''.join(filter(str.isdigit, phone_number))
 
-    # Check if the cleaned phone number contains only digits
-    if cleaned_number.isdigit():
-        # Check if the length of the cleaned number is within an acceptable range
-        if 7 <= len(cleaned_number) <= 15:
-            return True
-    return False
+    # Check if the cleaned phone number contains only digits and is between 7 and 15 digits long
+    return cleaned_number.isdigit() and 7 <= len(cleaned_number) <= 15
 
 def add_customer(first_name_entry, last_name_entry, phone_number_entry, email_entry, listbox):
     first_name = first_name_entry.get()
@@ -48,14 +41,16 @@ def add_customer(first_name_entry, last_name_entry, phone_number_entry, email_en
     if not first_name or not last_name:
         # Required fields are empty
         messagebox.showerror("Error", "First Name and Last Name are required fields.")
-    elif not is_valid_phone_number(phone_number):
+        return
+    if not is_valid_phone_number(phone_number):
         # Phone number contains non-numeric characters
         messagebox.showerror("Error", "Phone Number must be valid.")
-    elif not is_valid_email(email):
+        return
+    if not is_valid_email(email):
         # Email is not in a valid format
         messagebox.showerror("Error", "Invalid Email Address.")
-    else:
-        # All checks passed, add the customer to the database
+        return
+    try:
         cursor.execute('''
             INSERT INTO Customers (first_name, last_name, phone_number, email)
             VALUES (?, ?, ?, ?)
@@ -70,6 +65,8 @@ def add_customer(first_name_entry, last_name_entry, phone_number_entry, email_en
         email_entry.delete(0, tk.END)
 
         messagebox.showinfo("Success", "Customer added successfully.")
+    except sqlite3.Error as error:
+        messagebox.showerror("Error", f"Error adding customer: {error}")
 
 # Function to edit a customer
 def modify_customer(customer_listbox):
@@ -80,11 +77,9 @@ def modify_customer(customer_listbox):
     if not selected_index:
         messagebox.showerror("Error", "Please select a customer to edit.")
         return
-
-    # Extract the customer_id from the selected item
-    customer_entry = customer_listbox.get(selected_index)
-
     try:
+        # Extract the customer_id from the selected item
+        customer_entry = customer_listbox.get(selected_index)
         customer_id = int(customer_entry.split("ID: ")[1].split(",")[0])
         existing_phone = customer_entry.split("Phone: ")[1].split(",")[0]
         existing_email = customer_entry.split("Email: ")[1].split(",")[0]
@@ -97,22 +92,26 @@ def modify_customer(customer_listbox):
     edit_window.title("Edit Customer")# Create an edit dialog window
     
     # Create Entry widgets for the new phone and email
-    phone_label = tk.Label(edit_window, text="New Phone:")
-    phone_label.pack()
-    new_phone_entry = tk.Entry(edit_window)
-    new_phone_entry.insert(0, existing_phone)
-    new_phone_entry.pack()
 
-    email_label = tk.Label(edit_window, text="New Email:")
-    email_label.pack()
-    new_email_entry = tk.Entry(edit_window)
-    new_email_entry.insert(0, existing_email)
-    new_email_entry.pack()
+    entries_and_labels = [
+        ("New Phone:", existing_phone),
+        ("New Email:", existing_email)
+    ]
+    entry_values = []
+
+    for label_text, default_value in entries_and_labels:
+        label = tk.Label(edit_window, text=label_text)
+        label.pack()
+
+        entry = tk.Entry(edit_window)
+        entry.insert(0, default_value)
+        entry.pack()
+        entry_values.append(entry)
 
     # Function to save changes and update the database
     def save_changes():
-        new_phone = new_phone_entry.get()
-        new_email = new_email_entry.get()
+        new_phone = entry_values[0].get()
+        new_email = entry_values[1].get()
 
         if not is_valid_phone_number(new_phone):
             messagebox.showerror("Error", "Invalid Phone Number.")
@@ -124,7 +123,6 @@ def modify_customer(customer_listbox):
 
         # Update the customer's phone and email in the database
         try:
-            # Update the customer's phone and email in the database
             cursor.execute('''
                 UPDATE Customers
                 SET phone_number = ?, email = ?
@@ -135,9 +133,9 @@ def modify_customer(customer_listbox):
             connection.commit()
         except sqlite3.Error as e:
             print("Error updating customer phone and email:", e)
+
         edit_window.destroy()
         refresh_customer_list(customer_listbox)
-
         messagebox.showinfo("Success", "Customer updated successfully.")
 
     # Button to save changes
@@ -150,7 +148,9 @@ def modify_customer(customer_listbox):
 def refresh_customer_list(listbox):
     cursor.execute('SELECT * FROM Customers')
     customers = cursor.fetchall()
+
     listbox.delete(0, tk.END)
+    
     for customer in customers:
         customer_info = f"ID: {customer[0]}, Name: {customer[1]} {customer[2]}, Phone: {customer[3]}, Email: {customer[4]}"
         listbox.insert(tk.END, customer_info)
