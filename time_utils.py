e84e99a1d59c79f1db02df9cb1c34f49aaaf3cb2
Charles Law <calaw726@gmail.com>
2023-11-30 14:44:10 -0500

    Allowed get_time fcn to return either the date and time, or just date

--- time_utils.py
+++ time_utils.py
@@ -15,13 +15,17 @@ def get_selected_date(calendar, date_entry, hour_spinbox, minute_spinbox, check)
             return
 
     # Get selected time from the Spinboxes
-    selected_hour = int(hour_spinbox.get())
-    selected_minute = int(minute_spinbox.get())
+    if hour_spinbox and minute_spinbox:
+        selected_hour = int(hour_spinbox.get())
+        selected_minute = int(minute_spinbox.get())
 
-    # Combine date and time in the desired format (MM/DD/YYYY HH:MM)
-    formatted_date = selected_date.strftime('%m/%d/%Y')
-    selected_datetime = f"{formatted_date} {selected_hour:02d}:{selected_minute:02d}"
-
+        # Combine date and time in the desired format (MM/DD/YYYY HH:MM)
+        formatted_date = selected_date.strftime('%m/%d/%Y')
+        selected_datetime = f"{formatted_date} {selected_hour:02d}:{selected_minute:02d}"
+    else:
+        # Combine date and time in the desired format (MM/DD/YYYY)
+        selected_datetime = selected_date.strftime('%m/%d/%Y')
+    
     # Update the date entry widget
     date_entry.delete(0, tk.END)
     date_entry.insert(0, selected_datetime)
@@ -31,3 +35,6 @@ def get_selected_date(calendar, date_entry, hour_spinbox, minute_spinbox, check)
 def format_date_time(date_time):
     formatted_date = datetime.strptime(date_time, "%Y-%m-%d").strftime("%d/%m/%Y %H:%M")
     return formatted_date
+
+def get_curr_date():
+    return datetime.now().strftime('%m/%d/%Y')
